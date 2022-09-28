8767a65a62248483559fb47f9cfcb2cc8f429fd7
Shade Wang <b7ack42@gmail.com>
2022-09-28 01:19:24 -0700

    adjust

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-paru -S chezmoi xorg bspwm picom-animations-git kitty eww rofi dunst redshift pamixer xorg-xinit networkmanager noto-fonts nerd-fonts-noto-sans-mono ttf-font-awesome brightnessctl cava glava neofetch neovim sxhkd betterlockscreen flameshot feh playerctl jq recode moreutils jgmenu xcolor bluez-utils
+yay -S chezmoi xorg bspwm picom-animations-git kitty eww rofi dunst redshift pamixer xorg-xinit networkmanager noto-fonts nerd-fonts-noto-sans-mono ttf-font-awesome brightnessctl cava glava neofetch neovim sxhkd betterlockscreen flameshot feh playerctl jq recode moreutils jgmenu xcolor bluez-utils papirus-icon-theme 
 chezmoi init --apply https://github.com/b7ack42/bspwm_dotfiles.git
